bd93a277aade7d9c41ec1de0ba98014ea2135015
Matt <mbauer1221@gmail.com>
2022-11-16 17:00:30 -0600

    All MVC CRUD operations functional

--- src/main/java/net/yorksolutions/mattbauercapstonebe/controllers/ResponseController.java
+++ src/main/java/net/yorksolutions/mattbauercapstonebe/controllers/ResponseController.java
@@ -1,12 +1,13 @@
 package net.yorksolutions.mattbauercapstonebe.controllers;
 
+import net.yorksolutions.mattbauercapstonebe.modules.FinishedProcess;
 import net.yorksolutions.mattbauercapstonebe.services.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/response")
+@CrossOrigin
 public class ResponseController {
 
     ResponseService responseService;
@@ -15,4 +16,17 @@ public class ResponseController {
     public ResponseController(ResponseService responseService) {
         this.responseService = responseService;
     }
+
+    @PostMapping
+    public FinishedProcess create(@RequestBody FinishedProcess process){
+        return this.responseService.create(process);
+    }
+
+    @GetMapping
+    public Iterable<FinishedProcess> getAll(){
+        return this.responseService.getall();
+    }
+
+
 }
+
--- src/main/java/net/yorksolutions/mattbauercapstonebe/services/ResponseService.java
+++ src/main/java/net/yorksolutions/mattbauercapstonebe/services/ResponseService.java
@@ -1,5 +1,6 @@
 package net.yorksolutions.mattbauercapstonebe.services;
 
+import net.yorksolutions.mattbauercapstonebe.modules.FinishedProcess;
 import net.yorksolutions.mattbauercapstonebe.repositories.ResponseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,13 @@ public class ResponseService {
     @Autowired
     public ResponseService(ResponseRepository responseRepository) {
         this.responseRepository = responseRepository;
+    }
+
+    public FinishedProcess create(FinishedProcess process) {
+        return this.responseRepository.save(process);
+    }
+
+    public Iterable<FinishedProcess> getall() {
+        return this.responseRepository.findAll();
     }
 }
